8513386c66d1ee9089b8ac0176b0576dc0d9456e
boushi-bird <boushi-bird@users.noreply.github.com>
2020-02-10 17:09:46 +0900

    URL変更案内を表示

--- 3594t-deck/service-worker.js
+++ 3594t-deck/service-worker.js
@@ -13,21 +13,8 @@
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts(
-  "scripts/precache-manifest.9f8a205723b7e7dcc3afc5f53ed0dade.js"
-);
-
 workbox.core.skipWaiting();
 
 workbox.core.clientsClaim();
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.registerRoute(/\.md5$/gi, new workbox.strategies.NetworkFirst(), 'GET');
-workbox.routing.registerRoute(/^https:\/\/3594t\.net\/img\/.*\.(jpg|png|gif)$/gi, new workbox.strategies.CacheFirst({ "cacheName":"3594t.net/img", plugins: [new workbox.expiration.Plugin({ maxAgeSeconds: 259200, purgeOnQuotaError: false })] }), 'GET');
+workbox.precaching.precacheAndRoute([], {});
